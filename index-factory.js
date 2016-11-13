6fca3fb2e4ff015001f97ec4814d4adab0df24ce
Mitch Allen <mitch.c.allen@gmail.com>
2016-11-13 04:25:37 -0700

    added goto method and tests

--- index-factory.js
+++ index-factory.js
@@ -13,24 +13,49 @@ module.exports.create = (spec) => {
     }
     // private 
     let _package = "@mitchallen/pen";
+    let _verbose = spec.verbose || false;
     // some pen properties can't be changed once set
     let _color = spec.color || 0x000000;
     let _width = spec.width || 1;
     let _alpha = spec.alpha || 1.0;
 
-    var _penDown = false;
+    var _penDown = false,
+        _path = [];
 
     return {
         // public 
         package: () => _package,
         health: () => "OK",
-        
+
         color: () => _color,
         width: () => _width,
         alpha: () => _alpha,
+        path: () => _path,
 
         penDown: () => _penDown = true,
         penUp: () => _penDown = false,
         isPenDown: () => _penDown,
+
+        goto: (point) => {
+            if(!point) {
+                return null;
+            }
+            if(!point.x || !point.y) {
+                console.error("pen.goto: point must contain x and y values");
+                return null;
+            }
+
+            var op = _penDown ? "L" : "M";
+
+            if(_path.length == 0 && op != "M" ) {
+                if(_verbose) {
+                    // Insert starting point
+                    console.warn("pen.goto inserting default MoveTo starting point");
+                }
+                _path.push( { op: "M", x: 0, y: 0 } );
+            }
+
+            _path.push( { op: op, x: point.x, y: point.y } ); 
+        }
     };
 };
--- test/smoke-test-factory.js
+++ test/smoke-test-factory.js
@@ -97,4 +97,52 @@ describe('module factory smoke test', () => {
         pen.isPenDown().should.eql(false);
         done();
     });
+
+    it('goto while pen up should set path MoveTo (M) op', done => {
+        var pen = _factory.create({});
+        let px = 10, py = 20;
+        pen.goto( { x: px, y: py } );
+        let path = pen.path();
+        // console.log(path);
+        let el = path[0];
+        el.op.should.eql("M");
+        el.x.should.eql(px);
+        el.y.should.eql(py);
+        done();
+    });
+
+    it('goto while pen down should set path LineTo (L) op', done => {
+        var pen = _factory.create({});
+        let x1 = 10, y1 = 20;
+        pen.goto( { x: x1, y: y1 } );
+        pen.penDown();
+        let x2 = 30, y2 = 40;
+        pen.goto( { x: x2, y: y2 } );
+        let path = pen.path();
+        // console.log(path);
+        let el = path[1];
+        el.op.should.eql("L");
+        el.x.should.eql(x2);
+        el.y.should.eql(y2);
+        done();
+    });
+
+    it('goto while pen down and path empty should inser MoveTo (M) op', done => {
+        var pen = _factory.create({});
+        pen.penDown();
+        let px = 15, py = 25;
+        pen.goto( { x: px, y: py } );
+        let path = pen.path();
+        // console.log(path);
+        path.length.should.eql(2,"path should contain 2 operations");
+        let el0 = path[0];
+        el0.op.should.eql("M");
+        el0.x.should.eql(0);
+        el0.y.should.eql(0);
+        let el1 = path[1];
+        el1.op.should.eql("L");
+        el1.x.should.eql(px);
+        el1.y.should.eql(py);
+        done();
+    });
 });
